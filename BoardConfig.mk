53c57cdc69bac6a9bab56b9aa6f2438e3ef13e79
Martinusbe <martinusbe@gmail.com>
2016-05-03 14:17:20 +0200

    bc: uber 4.9 & enable some opts
    Change-Id: Ibae31d242886c0dd1272ca22895e4b45d8c2e951
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,14 +44,14 @@ TARGET_BOARD_INFO_FILE ?= $(PLATFORM_PATH)/board-info.txt
 # Tipsy additions
 BLOCK_BASED_OTA=false
 # Optimizations
-CLANG_O3 := false
+CLANG_O3 := true
 STRICT_ALIASING := false
-KRAIT_TUNINGS := false
+KRAIT_TUNINGS := true
 GRAPHITE_OPTS := false
-ENABLE_GCCONLY := false
+ENABLE_GCCONLY := true
 
 # Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8-uber/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 # Rom Toolchain
 TARGET_GCC_VERSION_EXP := 4.9-uber
