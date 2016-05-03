ab3a225c412dabbccf4d0437797c9b85d4df5ee8
Martinusbe <martinusbe@gmail.com>
2016-05-03 14:17:20 +0200

    idk wth just happened here?

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,11 @@ ENABLE_GCCONLY := true
 
 # Kernel Toolchain
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
+<<<<<<< HEAD
 #KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+=======
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+>>>>>>> 53c57cd... bc: uber 4.9 & enable some opts
 # Rom Toolchain
 TARGET_GCC_VERSION_EXP := 4.9-uber
 
